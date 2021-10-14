7136dede52f893382ae7013ef4efaa73d5937737
tanagaku <aegidousi@outlook.jp>
2021-10-14 22:47:55 +0900

    htmlファイルをテンプレートとして使用できるよう改修

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -3,21 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう!
-				</body>
-			</html>
-		`))
+//templ1は１つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+
+	if err := t.templ.Execute(w, nil); err != nil {
+		log.Fatal("templateHandleError", err)
+	}
+}
+
+func main() {
+	//ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	//webサーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
