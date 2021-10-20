46b4281dc29d3821469977b38d7e84db92c3dab4
tanagaku <aegidousi@outlook.jp>
2021-10-20 22:23:16 +0900

    チャットルーム生成の実装

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -28,12 +28,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	//ルート
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-
+	http.Handle("/room", r)
+	//チャットルームを開始
+	go r.run()
 	//webサーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-
 }
