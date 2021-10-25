da5da6bdfff474daaf0265208fc6c226f87fb38d
tanagaku <aegidousi@outlook.jp>
2021-10-25 23:17:35 +0900

    loginページの追加

---
+++ chapter2/chat/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sample_go_learning/chapter1/trace"
+	"sync"
+	"text/template"
+)
+
+//templ1は１つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once //複数から呼び出されても関数が1度しか実行されないことを保証する
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	if err := t.templ.Execute(w, r); err != nil {
+		log.Fatal("templateHandleError", err)
+	}
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() //フラグを解釈
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.Handle("/room", r)
+	//チャットルームを開始
+	go r.run()
+	log.Println("webサーバーを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ chapter2/trace/tracer_test.go
@@ -0,0 +1,24 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです。")
+	} else {
+		tracer.Trace("こんにちは、Traceパッケージ")
+		if buf.String() != "こんにちは、Traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
+}
+
+func TestOff(t *testing.T) {
+	var silentTracer Tracer = Off()
+	silentTracer.Trace("データ")
+}
