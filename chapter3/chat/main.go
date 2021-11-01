07ae772653b400e5037b047509a1a05c8d6cddad
tanagaku <aegidousi@outlook.jp>
2021-11-01 21:49:26 +0900

    アイコンを取得して画面表示できるよう変更

---
+++ chapter3/chat/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sample_go_learning/chapter2/trace"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+//templ1は１つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once //複数から呼び出されても関数が1度しか実行されないことを保証する
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() //フラグを解釈
+	//Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("セキュリティーキー")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/google"),
+	)
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+	http.Handle("/room", r)
+	//チャットルームを開始
+	go r.run()
+	log.Println("webサーバーを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ chapter3/chat/message.go
@@ -0,0 +1,9 @@
+package main
+
+//messageは1つのメッセージを表します
+type message struct {
+	Name      string
+	Message   string
+	When      string
+	AvatarURL string
+}
