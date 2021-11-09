4407eb74e889980b1bc271c604bf688aee7a7780
tanagaku <aegidousi@outlook.jp>
2021-11-09 20:27:56 +0900

    変換用のtemplateYamlを作成

--- chapter4/sprinkle/main.go
+++ chapter4/sprinkle/main.go
@@ -3,29 +3,31 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
-const otherWord = "*"
-
-var transforms = []string{
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
-	"lets" + otherWord,
-}
+const otherWord = "otherWord"
+const transformYaml = "transform.yaml"
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+	b, err := ioutil.ReadFile(transformYaml)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	transforms := make([]string, 0) //項目が増えた場合構造体にする
+	err = yaml.Unmarshal(b, &transforms)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		t := transforms[rand.Intn(len(transforms))]
