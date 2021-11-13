bd9c5b52e722f0de308e6b0329b9c77226c842d0
tanagaku <aegidousi@outlook.jp>
2021-11-13 16:10:58 +0900

    重複順列に修正

--- make_ten/main.go
+++ make_ten/main.go
@@ -54,14 +54,14 @@ func validation(args []string) ([]string, error) {
 
 func make_ten(nums []string) (string, error) {
 
-	opeNums := []int{1, 2, 3, 4}
-	for _, v := range Permute(opeNums) {
+	opes := []string{plus, minus, multiply, division}
+	for _, v := range PermuteWithRepetition(opes, 3) {
 		ope := nums[0] + v[0] + nums[1] + v[1] + nums[2] + v[2] + nums[3]
+
 		tv, err := types.Eval(token.NewFileSet(), nil, token.NoPos, ope)
 		if err != nil {
 			return "", errors.New(err.Error())
 		}
-
 		if tv.Value.String() == "10" {
 			return ope + "=10", nil
 		}
@@ -69,65 +69,20 @@ func make_ten(nums []string) (string, error) {
 	return "", nil
 }
 
-func Permute(nums []int) [][]string {
-	n := factorial(len(nums))
-	ret := make([][]int, 0, n)
-	permute(nums, &ret)
+//重複順列を返す
+func PermuteWithRepetition(opes []string, getn int) [][]string {
+	if getn == 0 {
+		return [][]string{{}}
+	}
 
-	strs := make([][]string, len(ret), n)
-	for i, vals := range ret {
-		for _, v := range vals {
-			strs[i] = append(strs[i], makeOpe(v))
+	results := [][]string{}
+	for i := 0; i < len(opes); i++ {
+		childPatterns := PermuteWithRepetition(opes, getn-1)
+		for _, chilchildPattern := range childPatterns {
+			pattern := append([]string{opes[i]}, chilchildPattern...)
+			results = append(results, pattern)
 		}
 	}
-	return strs
-}
 
-func permute(nums []int, ret *[][]int) {
-	*ret = append(*ret, makeCopy(nums))
-
-	n := len(nums)
-	p := make([]int, n+1)
-	for i := 0; i < n+1; i++ {
-		p[i] = i
-	}
-	for i := 1; i < n; {
-		p[i]--
-		j := 0
-		if i%2 == 1 {
-			j = p[i]
-		}
-
-		nums[i], nums[j] = nums[j], nums[i]
-		*ret = append(*ret, makeCopy(nums))
-		for i = 1; p[i] == 0; i++ {
-			p[i] = i
-		}
-	}
-}
-
-func factorial(n int) int {
-	ret := 1
-	for i := 2; i <= n; i++ {
-		ret *= i
-	}
-	return ret
-}
-
-func makeCopy(nums []int) []int {
-	return append([]int{}, nums...)
-}
-
-func makeOpe(i int) string {
-	switch i {
-	case 1:
-		return plus
-	case 2:
-		return minus
-	case 3:
-		return multiply
-	case 4:
-		return division
-	}
-	return "" //ココには来ない
+	return results
 }
