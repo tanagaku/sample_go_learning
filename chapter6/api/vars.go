e3019b57d100ff41af809db53097362a4e9dc990
tanagaku <aegidousi@outlook.jp>
2021-12-20 21:00:56 +0900

    chapter6の作成

---
+++ chapter6/api/vars.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+)
+
+var (
+	varsLock sync.RWMutex
+	vars     map[*http.Request]map[string]interface{}
+)
+
+func OpenVars(r *http.Request) {
+	varsLock.Lock()
+	if vars == nil {
+		vars = map[*http.Request]map[string]interface{}{}
+	}
+	vars[r] = map[string]interface{}{}
+	varsLock.Unlock()
+}
