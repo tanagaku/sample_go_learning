9b8900dd0ba0a609545ab99035b89654b102b37a
tanagaku <aegidousi@outlook.jp>
2021-12-29 18:20:21 +0900

    add main go

---
+++ chapter6/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
+
+func main() {}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			//respondErr(w, r, http.StatusUnauthorized, "不正なAPIきーです")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVar(r, "db", thisDb.DB("ballots"))
+	}
+}
