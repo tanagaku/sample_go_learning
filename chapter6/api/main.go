c539ba4ac0e47a7c3c92591d64bb9ac134fd3b97
tanagaku <aegidousi@outlook.jp>
2022-01-01 11:20:10 +0900

    add CQRS

--- chapter6/api/main.go
+++ chapter6/api/main.go
@@ -29,3 +29,19 @@ func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
 		SetVar(r, "db", thisDb.DB("ballots"))
 	}
 }
+
+func withVars(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		OpenVars(r)
+		defer CloseVars(r)
+		fn(w, r)
+	}
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
