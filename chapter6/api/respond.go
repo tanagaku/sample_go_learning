6f3b0d8b75e1161778f37d3da5c66628cc822e70
tanagaku <aegidousi@outlook.jp>
2022-01-02 22:04:04 +0900

    encoderの追加

---
+++ chapter6/api/respond.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func decodeBody(r *http.Request, v interface{}) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func encodeBody(w http.ResponseWriter, r *http.Request, v interface{}) error {
+	return json.NewEncoder(w).Encode(v)
+}
