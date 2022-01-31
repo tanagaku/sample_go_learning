3f87b47e129c0f2efa108c2727eac063f25026f0
tanagaku <aegidousi@outlook.jp>
2022-01-31 21:57:08 +0900

    テストコードの追加#

---
+++ chapter7/go.mod
@@ -0,0 +1,8 @@
+module sample_go_learning/chapter7
+
+go 1.17
+
+require (
+	github.com/cheekybits/is v0.0.0-20150225183255-68e9c0620927
+	github.com/oreilly-japan/go-programming-blueprints v0.0.0-20170214022001-a99fad583e18
+)
---
+++ chapter7/meander/cost_level_test.go
@@ -0,0 +1,18 @@
+package meander_test
+
+import (
+	"sample_go_learning/chapter7/meander"
+	"testing"
+
+	"github.com/cheekybits/is"
+)
+
+func TestCostValues(t *testing.T) {
+	is := is.New(t)
+
+	is.Equal(int(meander.Cost1), 1)
+	is.Equal(int(meander.Cost2), 2)
+	is.Equal(int(meander.Cost3), 3)
+	is.Equal(int(meander.Cost4), 4)
+	is.Equal(int(meander.Cost5), 5)
+}
