88d48a6aaa9db8ca7ad580730662a8220c3815e0
tanagaku <aegidousi@outlook.jp>
2022-02-08 21:01:05 +0900

    main関数の修正

--- chapter7/meander/cmd/main.go
+++ chapter7/meander/cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/tanagaku/sample_go_learning/chapter7/meander"
 )
@@ -16,6 +18,17 @@ func main() {
 		respond(w, r, meander.Journeys)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	})
 }
 
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
